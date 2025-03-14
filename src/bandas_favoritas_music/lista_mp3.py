3055a9efacf81147a15fd9c27bd8802abc220c18
Marcelo Vasselai <mvasselai@gmail.com>
2025-03-14 16:25:45 -0300

    Terceiro Commit

---
+++ src/bandas_favoritas_music/lista_mp3.py
@@ -0,0 +1,25 @@
+import yt_dlp
+
+def download_audio(video_url):
+    ydl_opts = {
+        'format': 'bestaudio/best',  # Baixar a melhor qualidade de áudio
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',  # Usar FFmpeg para extrair o áudio
+            'preferredcodec': 'mp3',  # Converter para MP3
+            'preferredquality': '192',  # Qualidade do MP3
+        }],
+        'outtmpl': './downloads/%(title)s.%(ext)s',  # Diretório de saída
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([video_url])
+
+if __name__ == '__main__':
+    video_urls = [
+        'https://www.youtube.com/watch?v=x34icYC8zA0',
+        # 'https://www.youtube.com/watch?v=VIDEO_ID_2',
+        # Adicione mais URLs conforme necessário
+    ]
+
+    for url in video_urls:
+        download_audio(url)
