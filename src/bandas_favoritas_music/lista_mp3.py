c5bf70097efdf09ee9e18899514627afa8cfca06
Marcelo Vasselai <mvasselai@gmail.com>
2025-03-14 18:27:09 -0300

    final

--- src/bandas_favoritas_music/lista_mp3.py
+++ src/bandas_favoritas_music/lista_mp3.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     video_urls = [
         'https://www.youtube.com/watch?v=x34icYC8zA0',
         # 'https://www.youtube.com/watch?v=VIDEO_ID_2',
-        # Adicione mais URLs conforme necessário
+        # Adicione mais URLs conforme necessário.
     ]
 
     for url in video_urls:
